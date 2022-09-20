ffed52eee678450dce8978f2bcb0ba8f6b672a9d
kksusa <kksusa1@gmail.com>
2022-09-20 21:46:03 +0500

    Создание метода вывода массива

--- Program.cs
+++ Program.cs
@@ -21,3 +21,15 @@ string[] InputArray(string[] array)
     }  
     return array;
 } 
+
+void PrintArray(string[] array)  
+{  
+    Console.Write("[");  
+    for (var i = 0; i < array.Length; i++)  
+    {  
+        Console.Write(array[i]);  
+        if (i != array.Length - 1) Console.Write(", ");  
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}  
